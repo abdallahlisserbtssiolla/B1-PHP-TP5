96f1f985435a58e6e456e187c6e1c46b4c044ae5
Abdallah Lisser BTS SIO LLA <144156904+abdallahlisserbtssiolla@users.noreply.github.com>
2023-12-06 13:19:34 +0100

    Add files via upload

--- calculerNbTotalPassagers.php
+++ calculerNbTotalPassagers.php
@@ -1,3 +1,11 @@
 <?php
+$nbPassagers = array(35, 10, 22, 52, 15, 58, 8, 73);
 
+$totalPassagers = 0;
+
+for ($i = 0, $nbTraversees = count($nbPassagers); $i < $nbTraversees; $i++) {
+    $totalPassagers =  $totalPassagers + $nbPassagers[$i];
+}
+
+echo $totalPassagers , " passagers transportés le 22/09/2022.\n";
 ?>
